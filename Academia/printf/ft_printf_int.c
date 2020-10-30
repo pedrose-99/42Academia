c136fe03d6d0ad4c07076e5cd41d398fbd6e809d
Pedro <pedserra.99@gmail.com>
2020-10-30 11:05:08 +0100

    printf bonito

--- Academia/printf/ft_printf_int.c
+++ Academia/printf/ft_printf_int.c
@@ -1,37 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_unsig.c                                   :+:      :+:    :+:   */
+/*   ft_printf_int.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pserrano <pserrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/23 12:58:41 by pserrano          #+#    #+#             */
-/*   Updated: 2020/10/23 18:08:21 by pserrano         ###   ########.fr       */
+/*   Created: 2020/02/28 08:59:54 by pserrano          #+#    #+#             */
+/*   Updated: 2020/10/26 12:20:06 by pserrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	caseu_zero_o_prec(char *p, int tam_num, t_list *f)
+void	caseint_zero_o_prec(int i, char *p, int tam_num, t_list *f)
 {
+	p = print_menos(i, p, f);
 	if (f->precision > 0)
+	{
+		if (i < 0)
+			tam_num--;
 		print_zero(f->precision - tam_num, f);
+	}
 	else
 		print_zero(f->width - tam_num, f);
 	print_cosita(p, f);
 }
 
-void	caseu_zero_prec(char *p, int tam_num, t_list *f)
+void	caseint_zero_prec(int i, int tam_num, char *p, t_list *f)
 {
 	if (f->precision < tam_num)
+	{
 		print_space(f->width - tam_num, f);
+		if (f->precision > tam_num)
+			tam_num--;
+	}
+	else if (i < 0)
+		print_space(f->width - f->precision - 1, f);
 	else
 		print_space(f->width - f->precision, f);
+	if (i < 0)
+	{
+		p = print_menos(i, p, f);
+		tam_num = tam_num - 1;
+	}
 	print_zero(f->precision - tam_num, f);
 	print_cosita(p, f);
 }
 
-void	caseu_minus_prec(char *p, int i, int tam_num, t_list *f)
+void	caseint_minus_prec(int i, int tam_num, char *p, t_list *f)
 {
 	p = print_menos(i, p, f);
 	if (i < 0)
@@ -49,7 +65,7 @@ void	caseu_minus_prec(char *p, int i, int tam_num, t_list *f)
 		print_space((f->width - tam_num), f);
 }
 
-void	caseu_width_prec(int i, int tam_num, char *p, t_list *f)
+void	caseint_width_prec(int i, int tam_num, char *p, t_list *f)
 {
 	if (i < 0)
 	{
@@ -72,7 +88,7 @@ void	caseu_width_prec(int i, int tam_num, char *p, t_list *f)
 	print_cosita(p, f);
 }
 
-void	caseu_minus(char *p, int tam_num, int i, t_list *f)
+void	caseint_minus(int i, int tam_num, char *p, t_list *f)
 {
 	if (f->precision == 0 && i == 0)
 		print_space(f->width, f);
@@ -83,7 +99,7 @@ void	caseu_minus(char *p, int tam_num, int i, t_list *f)
 	}
 }
 
-void	caseu_width(char *p, int tam_num, int i, t_list *f)
+void	caseint_width(int i, int tam_num, char *p, t_list *f)
 {
 	if (f->precision == 0 && i == 0)
 		print_space(f->width, f);
@@ -94,32 +110,31 @@ void	caseu_width(char *p, int tam_num, int i, t_list *f)
 	}
 }
 
-void	print_unsigned(t_list *f)
+void	print_int(t_list *f)
 {
-	long long int			i;
-	int						tam_num;
-	char					*p;
+	int		i;
+	int		tam_num;
+	char	*p;
 
-	i = va_arg(f->ap, unsigned long int);
-	if (i == -1)
-		p = ft_itoa(4294967295);
-	else
-		p = ft_itoa(i);
+	i = va_arg(f->ap, int);
+	p = ft_itoa(i); 
 	tam_num = ft_strlen(p);
-	if ((f->zero > 0) && (f->precision > 0))
-		caseu_zero_prec(p, tam_num, f);
+	if (f->precision == 0 && i == 0 && f->width < 0 && f->minus < 0)
+		ft_printf("%s", "");
+	else if ((f->zero > 0) && (f->precision > 0))
+		caseint_zero_prec(i, tam_num, p, f);
 	else if (f->minus > 0 && f->precision <= 0)
-		caseu_minus(p, tam_num, i, f);
+		caseint_minus(i, tam_num, p, f);
 	else if ((f->zero > 0 && f->precision < 0)
 		|| (f->zero < 0 && f->precision > 0 && f->minus < 0 && f->width < 0))
-		caseu_zero_o_prec(p, tam_num, f);
+		caseint_zero_o_prec(i, p, tam_num, f);
 	else if ((f->minus < 0) && f->width > 0 && f->precision <= 0)
-		caseu_width(p, tam_num, i, f);
+		caseint_width(i, tam_num, p, f);
 	else if (f->minus > 0 && f->precision > 0)
-		caseu_minus_prec(p, i, tam_num, f);
+		caseint_minus_prec(i, tam_num, p, f);
 	else if (f->minus < 0 && f->width > 0 && f->precision > 0)
-		caseu_width_prec(i, tam_num, p, f);
-	else if (f->minus < 0 && f->width < 0 && f->precision < 0)
+		caseint_width_prec(i, tam_num, p, f);
+	else if (f->minus < 0 && f->width < 0 && f->precision <= 0)
 		print_cosita(p, f);
-	free(p);
+	free (p);
 }
